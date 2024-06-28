d8718352d8e873f07a1fdcc3263fab143ba5989f
Kübra Nur Gürsoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2024-06-28 14:22:54 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
