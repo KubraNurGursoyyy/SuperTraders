5170ab36faa3d03f8ae934d6f371fca048d56206
Kübra Nur Gürsoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2024-06-29 14:13:26 +0300

    Add Crud and HtmlResponse

---
+++ src/modules/shares/controller.js
@@ -0,0 +1,38 @@
+import models from "../../../database/models";
+import {errorHandling} from "../../utils/errorhandling";
+import respObject from "../../helper/responseobject";
+const httpStatus = require('http-status');
+
+//normal cretae fonkslarını yaz. sonra usercretae diye ayrı bir fonk ekleyeceksin onda kullanıcı yaratımı yapılacka.
+//o yaratımda da portfolye ve wallet istiyor mu diye sorulacak istemiyorsa yaratılmaycak.
+
+const create = async (req, res) => {
+    try {
+        let _b = req.body;
+        const created = await models.Share.create(_b);
+        if(created)
+            return res.status(httpStatus.CREATED).send(respObject(0,'Share created successfully.'));
+        else
+            return res.status(httpStatus.NOT_ACCEPTABLE).send(respObject(1,'Fail to create User.'));
+
+    }catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(respObject(1,'Failed',errorHandling(error)));
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const  deleted = await models.Share.destroy({
+            where:{ "id" : req.params.id },
+        });
+        if (deleted) {
+            return res.status(httpStatus.OK).send(respObject(0,'Successfully deleted',deleted));
+        }
+        return res.status(httpStatus.NOT_FOUND).send(respObject(1,'Failure','Failure in delete.'));
+
+    } catch (error) {
+        console.log(error);
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(respObject(1,'Failed',errorHandling(error)));
+    }
+
+}
---
+++ src/modules/tracerecords/controller.js
@@ -0,0 +1,35 @@
+import models from "../../../database/models";
+import {errorHandling} from "../../utils/errorhandling";
+import respObject from "../../helper/responseobject";
+const httpStatus = require('http-status');
+
+const create = async (req, res) => {
+    try {
+        let _b = req.body;
+        const created = await models.TraceRecord.create(_b);
+        if(created)
+            return res.status(httpStatus.CREATED).send(respObject(0,'TraceRecord created successfully.'));
+        else
+            return res.status(httpStatus.NOT_ACCEPTABLE).send(respObject(1,'Fail to create User.'));
+
+    }catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(respObject(1,'Failed',errorHandling(error)));
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const  deleted = await models.TraceRecord.destroy({
+            where:{ "id" : req.params.id },
+        });
+        if (deleted) {
+            return res.status(httpStatus.OK).send(respObject(0,'Successfully deleted',deleted));
+        }
+        return res.status(httpStatus.NOT_FOUND).send(respObject(1,'Failure','Failure in delete.'));
+
+    } catch (error) {
+        console.log(error);
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(respObject(1,'Failed',errorHandling(error)));
+    }
+
+}
