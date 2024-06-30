85cb6bf2db620a419ca36593f5cc1ba973d35b84
Kübra Nur Gürsoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2024-06-30 17:16:29 +0300

    Database And Models Error Fix

---
+++ database/config/config.js
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+    development: {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    },
+    test: {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    },
+    production: {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+};
+
+export default config;
