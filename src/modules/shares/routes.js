55a0a4482183d400cfc91ee23972b2f144e6b96d
Kübra Nur Gürsoy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2024-07-01 18:27:56 +0300

    add vercel json

--- src/modules/shares/routes.js
+++ src/modules/shares/routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import {buyShare, sellShare, updateSharePrice} from './controller.js';
+import {buyShare, getShares, sellShare, updateSharePrice} from './controller.js';
 
 const ShareRoutes = Router();
 
 ShareRoutes.route('/shares/updatePrice').post(updateSharePrice);
 ShareRoutes.route('/shares/buyShare').post(buyShare);
 ShareRoutes.route('/shares/sellShare').post(sellShare);
+ShareRoutes.route('/shares').get(getShares);
 
 export { ShareRoutes };
